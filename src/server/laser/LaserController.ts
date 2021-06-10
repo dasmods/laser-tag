3f29f9b400692cb0e2bb450d72b029d256099677
Jared <jared@jaredjohnson.dev>
2021-06-10 17:48:53 -0400

    Stub out LaserController

---
+++ src/server/laser/LaserController.ts
@@ -0,0 +1,5 @@
+export class LaserController {
+	static init() {}
+
+	private constructor() {}
+}
--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,3 +1,5 @@
 import { HealthPickupsController } from "server/health-pickups/HealthPickupsController";
+import { LaserController } from "server/laser/LaserController";
 
 HealthPickupsController.init();
+LaserController.init();
