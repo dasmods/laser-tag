4c25f3571ba2a7faaf8465159eca1e348dd2461a
Jared <jared@jaredjohnson.dev>
2021-06-21 12:46:45 -0400

    Remove cruft from TimeController

--- src/client/time/TimeController.ts
+++ src/client/time/TimeController.ts
@@ -1,16 +1,13 @@
-import { TimeModel } from "client/time/TimeModel";
+import { RunService } from "@rbxts/services";
 import { TimeService } from "shared/time/TimeService";
 import { ClientController } from "shared/util/controllers";
 
 const TIME_SERVICE = TimeService.getInstance();
 
-const onHeartbeat = () => {
-	TIME_SERVICE.sync();
-};
-
 export class TimeController extends ClientController {
 	init() {
-		const time = new TimeModel();
-		time.onHeartbeat(onHeartbeat);
+		RunService.Heartbeat.Connect(() => {
+			TIME_SERVICE.sync();
+		});
 	}
 }
